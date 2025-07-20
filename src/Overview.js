c679c54e129af65134e14712ee5c446bd2f0539d
JobMutugi <creativejobu@gmail.com>
2025-07-20 21:28:04 +0300

    add changes

--- src/Overview.js
+++ src/Overview.js
@@ -4,7 +4,7 @@ function Overview({ goals }) {
   return (
     <div className="overview">
       <h2>Savings Dashboard</h2>
-      {/* All statistics displays have been removed */}
+      {}
     </div>
   );
 }
